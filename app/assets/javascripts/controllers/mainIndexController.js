44f34c850462a27af638b975dc7f2d3522abe5c4
Ethan Gould <ethan.g.907@gmail.com>
2014-06-19 19:13:03 -0400

    Created 'ape' controller with seed data

---
+++ app/assets/javascripts/controllers/mainIndexController.js
@@ -0,0 +1,9 @@
+//create an idex controller
+var IndexController = function() {
+  $scope.apes = [
+    {species: "Gorilla", avg_height: 5, origin: "Africa", endangered: true},
+    {species: "Orangutan", avg_height: 5, origin: "Asia", endangered: true},
+    {species: "Chimpanzee", avg_height: 4, origin: "Africa", endangered: false},
+    {species: "Bonobos", avg_height: 4, origin: "Africa", endangered: false}
+  ];
+ };
