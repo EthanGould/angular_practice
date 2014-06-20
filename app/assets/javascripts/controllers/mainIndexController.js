1b33701665678051f1e100b0749df1acd2cffb10
Ethan Gould <ethan.g.907@gmail.com>
2014-06-19 20:33:53 -0400

    Added image attribute to apes

--- app/assets/javascripts/controllers/mainIndexController.js
+++ app/assets/javascripts/controllers/mainIndexController.js
@@ -1,9 +1,9 @@
 //create an idex controller
 var IndexController = function($scope) {
   $scope.apes = [
-    {species: "Gorilla", avg_height: 5, origin: "Africa", endangered: true},
-    {species: "Orangutan", avg_height: 5, origin: "Asia", endangered: true},
-    {species: "Chimpanzee", avg_height: 4, origin: "Africa", endangered: false},
-    {species: "Bonobos", avg_height: 4, origin: "Africa", endangered: false}
+    {species: "Gorilla", avg_height: 5, origin: "Africa", endangered: true, image: "images/gorilla.jpg"},
+    {species: "Orangutan", avg_height: 5, origin: "Asia", endangered: true, image: "images/orangutan.jpg"},
+    {species: "Chimpanzee", avg_height: 4, origin: "Africa", endangered: false, image: "images/chimpanzee.jpg"},
+    {species: "Bonobos", avg_height: 4, origin: "Africa", endangered: false, image: "images/bonobo.jpg"}
   ];
 };
